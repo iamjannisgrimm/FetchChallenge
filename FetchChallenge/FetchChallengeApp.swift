4b6b400763969c14dc99c646cd9772ad36f7406e
iamjannisgrimm <jannis@grimm.me>
2024-08-29 16:38:40 -0400

    Initial Commit

---
+++ FetchChallenge/FetchChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchChallengeApp.swift
+//  FetchChallenge
+//
+//  Created by Jannis Grimm on 8/29/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
